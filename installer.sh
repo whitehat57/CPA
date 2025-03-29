71b5d95a65cebd5ff6dac51dd3f1f8ca5ed3adfe
whitehat57 <86547977+whitehat57@users.noreply.github.com>
2025-03-29 12:48:18 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,73 +1,80 @@
 #!/bin/bash
 
-# CyberPeopleAttack Installer Script
-
-# Banner
+# CyberPeopleAttack Installer v2025
 clear
-echo '  ___  ____   __  '
-echo ' / __)(  _ \ / _\ '
-echo '( (__  ) __//    \'
-echo ' \___)(__)  \_/\_/ v.2025'
-echo '     Installer'
-echo '=========================='
+cat << 'EOF'
+=====================================
+   CyberPeopleAttack Installer v2025
+=====================================
+      _   _   _   _   _   _         
+     / \ / \ / \ / \ / \ / \       
+    ( C | P | A | I | N | S )       
+     \_/ \_/ \_/ \_/ \_/ \_/       
+=====================================
+EOF
 
-# Set variables
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin:$HOME/.local/bin:$HOME/.cargo/bin
+echo "[+] Starting full installer script..."
 
 # Update and install essential packages
+echo "[+] Updating package lists..."
 pkg update -y && pkg upgrade -y
-pkg install -y git curl wget python golang nodejs clang \
-    tsu proot termux-api nano unzip zip \
-    libffi openssl rust binutils
 
-# Setup pip & pipx
-python -m ensurepip --upgrade
+# Install core languages and tools
+echo "[+] Installing Python, Go, Node.js, and pipx..."
+pkg install -y python golang nodejs curl git zsh
+python -m ensurepip
 python -m pip install --upgrade pip
 python -m pip install pipx
 pipx ensurepath
 
-# Install Python libraries
-pipx install --pip-args="--no-cache-dir" aiohttp==3.11.14
-pipx inject aiohttp colorama==0.4.6 fake_useragent==2.1.0 Requests==2.32.3 urllib3==2.3.0
+# Set Go path properly
+mkdir -p ~/go/{bin,src,pkg}
+echo 'export GOPATH=$HOME/go' >> ~/.profile
+echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.profile
 
-# Install Node.js libraries
-yarn global add net http2 tls cluster url crypto user-agents fs header-generator fake-useragent https-proxy-agent || \
+# Install required Python libraries
+echo "[+] Installing Python libraries..."
+pip install aiohttp==3.11.14 colorama==0.4.6 fake_useragent==2.1.0 requests==2.32.3 urllib3==2.3.0
+
+# Install required Node.js libraries
+echo "[+] Installing Node.js libraries..."
 npm install -g net http2 tls cluster url crypto user-agents fs header-generator fake-useragent https-proxy-agent
 
-# Clone tools
-TOOL_DIR=$HOME/CPA_TOOLS
-mkdir -p "$TOOL_DIR"
-cd "$TOOL_DIR"
+# Clone required repositories
+echo "[+] Cloning project repositories..."
+mkdir -p ~/CPA-tools
+cd ~/CPA-tools
+git clone https://github.com/whitehat57/LOIC.git
+git clone https://oauth2:<your_token>@gitlab.com/whitehat57/cpa.git
+git clone https://oauth2:<your_token>@gitlab.com/whitehat57/karma-go.git
+git clone https://oauth2:<your_token>@gitlab.com/whitehat57/techstack.git
 
-REPOS=(
-  "https://github.com/whitehat57/LOIC.git"
-  "https://gitlab.com/whitehat57/cpa.git"
-  "https://gitlab.com/whitehat57/karma-go.git"
-  "https://gitlab.com/whitehat57/techstack.git"
-)
+# Set hacker-style font for Termux
+echo "[+] Setting hacker-style font for Termux..."
+mkdir -p ~/.termux
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
+curl -fsSL -o ~/.termux/font.ttf "$FONT_URL"
+echo "font_size=14" > ~/.termux/termux.properties
+termux-reload-settings
 
-for repo in "${REPOS[@]}"; do
-  git clone "$repo"
-done
+# Install Oh My Zsh and plugins
+echo "[+] Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Setup Termux startup banner
-BANNER_FILE="$PREFIX/etc/motd"
-cat > "$BANNER_FILE" <<EOF
-               ▒▒████████      ▒▒█████████      ▒▒████████
-              ▒▒██     ▒▒█      ▒▒██   ▒▒██      ▒▒██  ▒▒██
-             ▒▒██               ▒▒██   ▒▒██      ▒▒██  ▒▒██
-             ▒▒██               ▒▒██▒█████       ▒▒████████
-              ▒▒██     ▒▒█      ▒▒██             ▒▒██  ▒▒██
-               ▒▒████████      ▒▒███            ▒▒███  ▒▒██  
+ZSH_CUSTOM=~/.oh-my-zsh/custom
 
-           ---------------⚔️⚔️ CYBER PEOPLE ATTACK ⚔️⚔️---------------
-EOF
+echo "[+] Installing Zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
 
-# Set custom shell prompt
-SHELL_RC="$HOME/.bashrc"
-echo "\nexport PS1=\"\\[\\e[1;32m\\]CPA@free_Palestine > \\[$(tput sgr0)\\]\"" >> "$SHELL_RC"
+# Set Zsh as default shell and configure prompt
+echo "[+] Setting custom Zsh prompt..."
+sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)/' ~/.zshrc
+echo 'export PROMPT="%F{green}CPA@free_Palestine%f %1~ %# "' >> ~/.zshrc
 
-# Finish
-echo -e "\n[+] Installation complete!\n[!] Restart Termux or run 'source ~/.bashrc' to apply the changes."
-exit 0
+chsh -s zsh
+
+# Done
+echo "[+] Installation complete. Please restart Termux or run 'source ~/.zshrc'"
+echo "[!] Enjoy CyberPeopleAttack Mini Distro! ⚔️"
